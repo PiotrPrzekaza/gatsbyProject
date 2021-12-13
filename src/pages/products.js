00b01e18b52a554d023f87a2cc87f2991d29ae6f
Piotr <piotr.przekaza@hotmail.com>
2021-12-13 21:29:33 +0100

    fix

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,47 +1,44 @@
+/* eslint-disable react/prop-types */
 
-import { graphql } from 'gatsby'
-import React from 'react'
+import { graphql } from 'gatsby';
+import React from 'react';
 
 const Products = ({ data }) => {
-  const product = data.allDatoCmsProduct.edges
+  const product = data.allDatoCmsProduct.edges;
   return (
     <div>
       <h1>Wpisy</h1>
-      {
-        product.map(({ node }) => {
-          return (
-            <div key={node.slug}>
-              <a href={node.slug}>{node.title}</a>
-            </div>
-
-          )
-        })
-      }
+      {product.map(({ node }) => {
+        return (
+          <div key={node.slug}>
+            <a href={node.slug}>{node.title}</a>
+          </div>
+        );
+      })}
     </div>
-  )
-}
-
+  );
+};
 
 export const query = graphql`
-query{
-	allDatoCmsProduct{
-		edges{
-      node{
-        title
-        slug
-        content
-        img {
-          url
-          alt
-          fluid:fixed(height:400, width:400) {
-            height
-            width
+  query {
+    allDatoCmsProduct {
+      edges {
+        node {
+          title
+          slug
+          content
+          img {
+            url
+            alt
+            fluid: fixed(height: 400, width: 400) {
+              height
+              width
+            }
           }
         }
       }
     }
   }
-}
-`
+`;
 
 export default Products;
