94142a7635df9e2ecd543f5fd6f700b5b9e37967
Piotr <piotr.przekaza@hotmail.com>
2021-12-13 21:37:13 +0100

    fix eslint issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,24 @@
-// @ts-check
-import * as React from "react"
+import * as React from 'react';
 
 // styles
 const pageStyles = {
-  color: "#232129",
+  color: '#232129',
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Magiczna Petelka</title>
-      <h1 style={headingStyles}>
-        Wpisy
-      </h1>
-
+      <h1 style={headingStyles}>Wpisy</h1>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
