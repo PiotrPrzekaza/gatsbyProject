b827c5f8965260bf89c6dbe28506125b10c014b7
Piotr <piotr.przekaza@hotmail.com>
2021-12-28 16:11:01 +0100

    add static image and bild basic idea of 404page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,42 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const NotFoundPage = () => (
   <main>
     <title>Nic tu nie ma</title>
     <h1>Upss! Coś poszło nie tak!</h1>
     <div>
+      <div>
+        <StaticImage src="../images/magicznaLogo.svg" />
+      </div>
       <p>
         Nie ma takiej strony, lub jest w budowie! Kliknij poniżej aby wrócić do
         strony głównej
       </p>
     </div>
     <div>
-      <img src="" alt="" />
+      <StaticImage
+        src="../images/home4.jpg"
+        alt="please include an alt"
+        width={{ width: 400 }}
+        height={{ height: 400 }}
+      />
     </div>
     <Link to="/">Strona główna</Link>
   </main>
 );
+
+export const query = graphql`
+  {
+    file {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
 
 export default NotFoundPage;
